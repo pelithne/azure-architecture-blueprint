214f13d05aadcc4e434a917851a0192757b39c5d
Peter <peter.lithner@microsoft.com>
2024-03-25 16:23:15 +0100

    adding first template file structure

---
+++ terraform/modules/aks_config/main.tf
@@ -0,0 +1,29 @@
+data "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+}
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_service" "aks_cluster" {
+  metadata {
+    name = "lb-internal-service"
+    annotations = {
+      "service.beta.kubernetes.io/azure-load-balancer-internal" = "true"
+      "service.beta.kubernetes.io/azure-load-balancer-internal-subnet" = "lb-subnet"
+      "service.beta.kubernetes.io/azure-pls-create" = "true"
+    }
+  }
+  spec {
+    port {
+      port        = 8080
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
