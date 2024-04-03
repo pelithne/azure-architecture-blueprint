19aebfdec4c0c3e443a4b7fdb59c24678536f870
abengtss-max <ali_bengtsson@icloud.com>
2024-04-03 17:30:51 +0200

    bastion host comments added

--- terraform/modules/bastion_host/main.tf
+++ terraform/modules/bastion_host/main.tf
@@ -1,81 +1,85 @@
+// This block specifies the required providers and their sources.
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm" // Specifies the source of the Azure provider.
     }
   }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 0.14.9" // Specifies the minimum required version of Terraform.
 }
 
+// This block creates a public IP address resource in Azure.
 resource "azurerm_public_ip" "public_ip" {
-  name                = "${var.name}PublicIp"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  name                = "${var.name}PublicIp" // The name of the public IP address.
+  location            = var.location // The location where the public IP address will be created.
+  resource_group_name = var.resource_group_name // The name of the resource group in which to create the public IP address.
+  allocation_method   = "Static" // The allocation method for the public IP address. This is set to Static.
+  sku                 = "Standard" // The SKU of the public IP address. This is set to Standard.
 
   lifecycle {
       ignore_changes = [
-          tags
+          tags // This tells Terraform to ignore changes to tags after the resource is created.
       ]
   }
 }
 
+// This block creates a Bastion Host resource in Azure.
 resource "azurerm_bastion_host" "bastion_host" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
+  name                = var.name // The name of the Bastion Host.
+  location            = var.location // The location where the Bastion Host will be created.
+  resource_group_name = var.resource_group_name // The name of the resource group in which to create the Bastion Host.
+  tags                = var.tags // The tags to associate with the Bastion Host.
 
   ip_configuration {
-    name                 = "configuration"
-    subnet_id            = var.subnet_id
-    public_ip_address_id = azurerm_public_ip.public_ip.id
+    name                 = "configuration" // The name of the IP configuration.
+    subnet_id            = var.subnet_id // The ID of the subnet in which to create the Bastion Host.
+    public_ip_address_id = azurerm_public_ip.public_ip.id // The ID of the public IP address to associate with the Bastion Host.
   }
 
   lifecycle {
       ignore_changes = [
-          tags
+          tags // This tells Terraform to ignore changes to tags after the resource is created.
       ]
   }
 }
 
+// The following blocks are commented out. If uncommented, they would create diagnostic settings for the Bastion Host and the public IP address.
 /*
 resource "azurerm_monitor_diagnostic_setting" "settings" {
-  name                       = "DiagnosticsSettings"
-  target_resource_id         = azurerm_bastion_host.bastion_host.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
+  name                       = "DiagnosticsSettings" // The name of the diagnostic settings.
+  target_resource_id         = azurerm_bastion_host.bastion_host.id // The ID of the resource to which the diagnostic settings apply.
+  log_analytics_workspace_id = var.log_analytics_workspace_id // The ID of the Log Analytics workspace to which the diagnostic data will be sent.
 
   enabled_log {
-    category = "BastionAuditLogs"
+    category = "BastionAuditLogs" // Enables the collection of Bastion Audit Logs.
   }
 
   metric {
-    category = "AllMetrics"
+    category = "AllMetrics" // Enables the collection of all metrics.
   }
 }
 
 
 resource "azurerm_monitor_diagnostic_setting" "pip_settings" {
-  name                       = "DiagnosticsSettings"
-  target_resource_id         = azurerm_public_ip.public_ip.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
+  name                       = "DiagnosticsSettings" // The name of the diagnostic settings.
+  target_resource_id         = azurerm_public_ip.public_ip.id // The ID of the resource to which the diagnostic settings apply.
+  log_analytics_workspace_id = var.log_analytics_workspace_id // The ID of the Log Analytics workspace to which the diagnostic data will be sent.
 
   enabled_log {
-    category = "DDoSProtectionNotifications"
+    category = "DDoSProtectionNotifications" // Enables the collection of DDoS Protection Notifications.
   }
 
   enabled_log {
-    category = "DDoSMitigationFlowLogs"
+    category = "DDoSMitigationFlowLogs" // Enables the collection of DDoS Mitigation Flow Logs.
   }
 
   enabled_log {
-    category = "DDoSMitigationReports"
+    category = "DDoSMitigationReports" // Enables the collection of DDoS Mitigation Reports.
   }
 
   metric {
-    category = "AllMetrics"
+    category = "AllMetrics" // Enables the collection of all metrics.
   }
 }
 */
--- terraform/modules/bastion_host/variables.tf
+++ terraform/modules/bastion_host/variables.tf
@@ -1,28 +1,40 @@
+// This block declares a variable named "resource_group_name". 
+// This variable is required and should specify the name of the resource group for the bastion host.
 variable "resource_group_name" {
   description = "(Required) Specifies the resource group name of the bastion host"
   type        = string
 }
 
+// This block declares a variable named "name". 
+// This variable is required and should specify the name of the bastion host.
 variable "name" {
   description = "(Required) Specifies the name of the bastion host"
   type        = string
 }
 
+// This block declares a variable named "location". 
+// This variable is required and should specify the location where the bastion host will be created.
 variable "location" {
   description = "(Required) Specifies the location of the bastion host"
   type        = string
 }
 
+// This block declares a variable named "tags". 
+// This variable is optional and should specify any tags to be associated with the bastion host.
 variable "tags" {
   description = "(Optional) Specifies the tags of the bastion host"
-  default     = {}
+  default     = {} // If no tags are provided, it defaults to an empty map.
 }
 
+// This block declares a variable named "subnet_id". 
+// This variable is required and should specify the ID of the subnet where the bastion host will be created.
 variable "subnet_id" {
   description = "(Required) Specifies subnet id of the bastion host"
   type        = string
 }
 
+// This block declares a variable named "log_analytics_workspace_id". 
+// This variable should specify the ID of the Log Analytics workspace where the diagnostic data will be sent.
 variable "log_analytics_workspace_id" {
   description = "Specifies the log analytics workspace id"
   type        = string
