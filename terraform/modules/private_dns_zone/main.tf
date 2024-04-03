b3cbda8f7fdaf8ca6f3a0811ea1068eaf5fc23c0
abengtss-max <ali_bengtsson@icloud.com>
2024-04-03 18:42:47 +0200

    added description for module private dns zone

--- terraform/modules/private_dns_zone/main.tf
+++ terraform/modules/private_dns_zone/main.tf
@@ -1,3 +1,5 @@
+# This block specifies the required providers for this Terraform configuration.
+# In this case, the Azure Resource Manager (azurerm) provider is required.
 terraform {
   required_providers {
     azurerm = {
@@ -5,14 +7,18 @@ terraform {
     }
   }
 
+  # This line specifies the minimum required version of Terraform for this configuration.
   required_version = ">= 0.14.9"
 }
 
+# This block defines a resource of type "azurerm_private_dns_zone".
+# This resource represents a private DNS zone in Azure.
 resource "azurerm_private_dns_zone" "private_dns_zone" {
   name                = var.name
   resource_group_name = var.resource_group_name
   tags                = var.tags
 
+  # This block specifies that changes to the "tags" attribute should be ignored during lifecycle operations.
   lifecycle {
     ignore_changes = [
       tags
@@ -20,6 +26,8 @@ resource "azurerm_private_dns_zone" "private_dns_zone" {
   }
 }
 
+# This block defines a resource of type "azurerm_private_dns_zone_virtual_network_link".
+# This resource represents a link between a private DNS zone and a virtual network in Azure.
 resource "azurerm_private_dns_zone_virtual_network_link" "link" {
   for_each = var.virtual_networks_to_link
 
@@ -28,6 +36,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "link" {
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone.name
   virtual_network_id    = "/subscriptions/${each.value.subscription_id}/resourceGroups/${each.value.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${each.key}"
 
+  # This block specifies that changes to the "tags" attribute should be ignored during lifecycle operations.
   lifecycle {
     ignore_changes = [
       tags
