3db592e6edbf89b38017f68ddaefb88d839c8943
abengtss-max <ali_bengtsson@icloud.com>
2024-04-03 19:22:20 +0200

    added description for vnet peering module

--- terraform/modules/virtual_network_peering/main.tf
+++ terraform/modules/virtual_network_peering/main.tf
@@ -1,3 +1,5 @@
+// This block specifies the required provider and version for this Terraform configuration. 
+// It requires the Azure provider (azurerm) version 3.50.0 or newer and Terraform version 0.14.9 or newer.
 terraform {
   required_providers {
     azurerm = {
@@ -9,16 +11,20 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+// This data block fetches the details of an existing Azure Virtual Network named vnet_1 in the specified resource group.
 data "azurerm_virtual_network" "vnet_1" {
   name                = var.vnet_1_name
   resource_group_name = var.vnet_1_rg
 }
 
+// This data block fetches the details of an existing Azure Virtual Network named vnet_2 in the specified resource group.
 data "azurerm_virtual_network" "vnet_2" {
   name                = var.vnet_2_name
   resource_group_name = var.vnet_2_rg
 }
 
+// This resource block creates a peering connection from vnet_1 to vnet_2. 
+// It allows traffic to be forwarded and allows access to the virtual network.
 resource "azurerm_virtual_network_peering" "peering" {
   name                      = var.peering_name_1_to_2
   resource_group_name       = var.vnet_1_rg
@@ -28,6 +34,8 @@ resource "azurerm_virtual_network_peering" "peering" {
   allow_forwarded_traffic      = true
 }
 
+// This resource block creates a peering connection from vnet_2 back to vnet_1. 
+// It allows traffic to be forwarded and allows access to the virtual network.
 resource "azurerm_virtual_network_peering" "peering-back" {
   name                      = var.peering_name_2_to_1
   resource_group_name       = var.vnet_2_rg
--- terraform/modules/virtual_network_peering/variables.tf
+++ terraform/modules/virtual_network_peering/variables.tf
@@ -1,39 +1,48 @@
+// This variable holds the name of the first virtual network.
 variable "vnet_1_name" {
   description = "Specifies the name of the first virtual network"
   type        = string
 }
 
+// This variable holds the resource id of the first virtual network.
 variable "vnet_1_id" {
   description = "Specifies the resource id of the first virtual network"
   type        = string
 }
 
+// This variable holds the resource group name of the first virtual network.
 variable "vnet_1_rg" {
   description = "Specifies the resource group name of the first virtual network"
   type        = string
 }
 
+// This variable holds the name of the second virtual network.
 variable "vnet_2_name" {
   description = "Specifies the name of the second virtual network"
   type        = string
 }
 
+
+// This variable holds the resource id of the second virtual network.
 variable "vnet_2_id" {
   description = "Specifies the resource id of the second virtual network"
   type        = string
 }
 
+// This variable holds the resource group name of the second virtual network.
 variable "vnet_2_rg" {
   description = "Specifies the resource group name of the second virtual network"
   type        = string
 }
 
+
+//  This variable holds the name of the peering from the first to the second virtual network. It's optional and defaults to "peering1to2" if not provided.
 variable "peering_name_1_to_2" {
   description = "(Optional) Specifies the name of the first to second virtual network peering"
   type        = string
   default     = "peering1to2"
 }
-
+// This variable holds the name of the peering from the second to the first virtual network. It's optional and defaults to "peering2to1" if not provided.
 variable "peering_name_2_to_1" {
   description = "(Optional) Specifies the name of the second to first virtual network peering"
   type        = string
